d3ef88f48d2c0d163cefd2df55543f5cbe36a499
komanda2 <komandapinf@gmail.com>
2018-11-23 17:30:33 +0300

    start6 23.11.18

--- js/my.js
+++ js/my.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-  
+$(document).ready(function(){}
+  /*
+          * Функция подсчета стоимости услуг на создания дизайна сайта
+          */
+          function calc() {
+              var domenname = document.getElementById("domenname"); //получаем ссылку на элемент Select (Тип дизайна)
+              var hostname = document.getElementById("hostname"); //получаем ссылку на чекбокс (Требуется верстка?)
+              var dev = document.getElementById("count"); //получаем ссылку на элемент input (Кол-во вариантов)
+              var total = document.getElementById("total"); //получаем ссылку на элемент span, в него будем писать стоимость дизайна
+
+
+              var price = 0;
+              price += parseInt(domenname.options[domenname.selectedIndex].value);
+              price += (hostname.checked == true) ? parseInt(hostname.value) : 0;
+              price = parseInt(dev.value) * price;
+
+              total.innerHTML = price;
+          }
 }
